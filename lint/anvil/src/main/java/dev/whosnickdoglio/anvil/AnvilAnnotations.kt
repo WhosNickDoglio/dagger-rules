6be57526787cdb799e03e40806d38f43a2a2e2c8
nicholasdoglio <NicholasDoglio@Gmail.com>
2023-03-31 16:02:15 -0400

    Add missing annotations and fix typo in another

--- lint/anvil/src/main/java/dev/whosnickdoglio/anvil/AnvilAnnotations.kt
+++ lint/anvil/src/main/java/dev/whosnickdoglio/anvil/AnvilAnnotations.kt
@@ -26,4 +26,4 @@ package dev.whosnickdoglio.anvil
 internal const val CONTRIBUTES_TO = "com.squareup.anvil.annotations.ContributesTo"
 internal const val CONTRIBUTES_BINDING = "com.squareup.anvil.annotations.ContributesBinding"
 internal const val CONTRIBUTES_MULTI_BINDING =
-    "com.squareup.anvil.annotations.ContributesMultiBinding"
+    "com.squareup.anvil.annotations.ContributesMultibinding"
--- lint/shared/src/main/java/dev/whosnickdoglio/lint/shared/AnnotationConstants.kt
+++ lint/shared/src/main/java/dev/whosnickdoglio/lint/shared/AnnotationConstants.kt
@@ -26,5 +26,7 @@ package dev.whosnickdoglio.lint.shared
 const val INJECT = "javax.inject.Inject"
 const val BINDS = "dagger.Binds"
 const val PROVIDES = "dagger.Provides"
-const val MULTIBINDS = "dagger.Multibinds"
+const val MULTIBINDS = "dagger.multibindings.Multibinds"
+const val INTO_MAP = "dagger.multibindings.IntoMap"
+const val INTO_SET = "dagger.multibindings.IntoSet"
 const val MODULE = "dagger.Module"
