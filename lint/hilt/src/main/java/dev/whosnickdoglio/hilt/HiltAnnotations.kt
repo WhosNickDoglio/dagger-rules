44c2b56a6dcdb9ba49e01ecfe811a72c63f51be6
nicholasdoglio <NicholasDoglio@Gmail.com>
2023-04-02 17:47:08 -0400

    Update MissingInstallInDetector to also work for @EntryPoint interfaces

--- lint/hilt/src/main/java/dev/whosnickdoglio/hilt/HiltAnnotations.kt
+++ lint/hilt/src/main/java/dev/whosnickdoglio/hilt/HiltAnnotations.kt
@@ -24,6 +24,7 @@
 package dev.whosnickdoglio.hilt
 
 internal const val INSTALL_IN = "dagger.hilt.InstallIn"
+internal const val ENTRY_POINT = "dagger.hilt.EntryPoint"
 internal const val ANDROID_ENTRY_POINT = "dagger.hilt.android.AndroidEntryPoint"
 internal const val HILT_ANDROID_APP = "dagger.hilt.android.HiltAndroidApp"
 internal const val HILT_VIEW_MODEL = "dagger.hilt.android.lifecycle.HiltViewModel"
