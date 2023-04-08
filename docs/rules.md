6a4005463a0a7714e5e1bc7b6305b7114179f6ce
nicholasdoglio <NicholasDoglio@Gmail.com>
2023-04-08 09:10:40 -0400

    Remove unstable rules from main for now

--- docs/rules.md
+++ docs/rules.md
@@ -2,10 +2,6 @@
 
 ## Prefer constructor injection over field injection
 
-## Prefer constructor injection over @Provides methods
-
-## Prefer using @Binds over @Provides
-
 ## Classes with @Provides or @Binds methods should be annotated with @Module
 
 ## A @Binds method parameter should be a subclass of it's return type
--- lint/dagger/src/main/java/dev/whosnickdoglio/dagger/DaggerRulesIssueRegistry.kt
+++ lint/dagger/src/main/java/dev/whosnickdoglio/dagger/DaggerRulesIssueRegistry.kt
@@ -10,8 +10,6 @@ import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
 import dev.whosnickdoglio.dagger.detectors.BindsWithCorrectReturnTypeDetector
 import dev.whosnickdoglio.dagger.detectors.ConstructorInjectionOverFieldInjectionDetector
-import dev.whosnickdoglio.dagger.detectors.ConstructorInjectionOverProvidesDetector
-import dev.whosnickdoglio.dagger.detectors.FavorBindsOverProvidesDetector
 import dev.whosnickdoglio.dagger.detectors.MissingModuleAnnotationDetector
 import dev.whosnickdoglio.dagger.detectors.StaticProvidesDetector
 
@@ -21,8 +19,6 @@ class DaggerRulesIssueRegistry : IssueRegistry() {
         listOf(
             BindsWithCorrectReturnTypeDetector.ISSUE,
             ConstructorInjectionOverFieldInjectionDetector.ISSUE,
-            ConstructorInjectionOverProvidesDetector.ISSUE,
-            FavorBindsOverProvidesDetector.ISSUE,
             MissingModuleAnnotationDetector.ISSUE,
             StaticProvidesDetector.ISSUE,
         )
