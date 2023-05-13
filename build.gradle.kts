48c1d56a2415f13734609f93d8a12fecd03a5b24
nicholasdoglio <NicholasDoglio@Gmail.com>
2023-05-13 08:40:30 -0400

    Remove Gradle Versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,27 +3,10 @@
  * SPDX-License-Identifier: MIT
  */
 
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-import java.util.*
-
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.lint) apply false
     alias(libs.plugins.kover) apply false
     alias(libs.plugins.doctor)
     alias(libs.plugins.dependencyAnalysis)
-    alias(libs.plugins.gradleVersions)
-}
-
-fun isNonStable(version: String): Boolean {
-    val unstableKeywords =
-        listOf("ALPHA", "RC", "BETA", "DEV", "-M").any { version.uppercase(Locale.getDefault()).contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    return unstableKeywords && !regex.matches(version)
-}
-
-tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
-    rejectVersionIf {
-        isNonStable(candidate.version)
-    }
 }
