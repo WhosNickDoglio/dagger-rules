fd871ec9ab6953c07cfbe1b7ebd2195bc7f577f6
nicholasdoglio <NicholasDoglio@Gmail.com>
2023-05-29 08:56:21 -0400

    Use default JDK for Detekt now that it supports JDK19

--- build-logic/src/main/java/lint.shared.gradle.kts
+++ build-logic/src/main/java/lint.shared.gradle.kts
@@ -71,8 +71,6 @@ configure<SpotlessExtension> {
 
 configure<KotlinJvmProjectExtension> { jvmToolchain(19) }
 
-tasks.withType<Detekt>().configureEach { jvmTarget = "11" }
-
 tasks.withType<JavaCompile>().configureEach { options.release.set(11) }
 
 tasks.withType<Test>().configureEach {
