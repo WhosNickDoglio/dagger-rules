5b4db241f48c39d7e2d854d1087e8066256bf8eb
nicholasdoglio <NicholasDoglio@Gmail.com>
2023-08-05 14:21:20 -0400

    Add Foojay Resolve settings plugin

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -21,3 +21,7 @@ dependencyResolutionManagement {
 
     versionCatalogs { create("libs") { from(files("../gradle/libs.versions.toml")) } }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,10 @@ dependencyResolutionManagement {
     }
 }
 
-plugins { id("com.gradle.enterprise") version ("3.14.1") }
+plugins {
+    id("com.gradle.enterprise") version ("3.14.1")
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+}
 
 gradleEnterprise {
     buildScan {
