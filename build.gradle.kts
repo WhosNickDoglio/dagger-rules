800e9330e7e1ff00ca58afe38b05b95f49984694
nicholasdoglio <NicholasDoglio@Gmail.com>
2023-09-03 00:01:13 -0400

    Minor build cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,6 @@
 plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.lint) apply false
-    alias(libs.plugins.kover) apply false
     alias(libs.plugins.doctor)
     alias(libs.plugins.dependencyAnalysis)
 }
