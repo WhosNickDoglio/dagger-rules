2141bc3339fbcedb978d50d097a5ab8e9852f086
nicholasdoglio <NicholasDoglio@Gmail.com>
2023-12-30 10:48:33 -0500

    Formatting

--- build-logic/src/main/kotlin/lint.shared.gradle.kts
+++ build-logic/src/main/kotlin/lint.shared.gradle.kts
@@ -3,18 +3,12 @@
  * SPDX-License-Identifier: MIT
  */
 
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.dependencies
-
-plugins {
-    id("rules.shared")
-}
+plugins { id("rules.shared") }
 
 val catalog =
     extensions.findByType(VersionCatalogsExtension::class.java) ?: error("No Catalog found!")
 
 val libs = catalog.named("libs")
-
 
 dependencies {
     "compileOnly"(libs.findBundle("lintApi").get())
