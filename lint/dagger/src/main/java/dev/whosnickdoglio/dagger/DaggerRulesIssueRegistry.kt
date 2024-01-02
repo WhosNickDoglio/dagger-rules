46e1b477ccd77357f78152b8d72973265ea0f05e
nicholasdoglio <NicholasDoglio@Gmail.com>
2024-01-02 17:24:50 -0500

    Add rule to warn against using scopes with assisted injection

--- lint/dagger/src/main/java/dev/whosnickdoglio/dagger/DaggerRulesIssueRegistry.kt
+++ lint/dagger/src/main/java/dev/whosnickdoglio/dagger/DaggerRulesIssueRegistry.kt
@@ -14,6 +14,7 @@ import dev.whosnickdoglio.dagger.detectors.ConstructorInjectionOverFieldInjectio
 import dev.whosnickdoglio.dagger.detectors.CorrectBindsUsageDetector
 import dev.whosnickdoglio.dagger.detectors.MissingModuleAnnotationDetector
 import dev.whosnickdoglio.dagger.detectors.MultipleScopesDetector
+import dev.whosnickdoglio.dagger.detectors.ScopedAssistedInjectedDetector
 import dev.whosnickdoglio.dagger.detectors.ScopedWithoutInjectAnnotationDetector
 import dev.whosnickdoglio.dagger.detectors.StaticProvidesDetector
 
@@ -29,6 +30,7 @@ class DaggerRulesIssueRegistry : IssueRegistry() {
             MissingModuleAnnotationDetector.ISSUE,
             MultipleScopesDetector.ISSUE,
             StaticProvidesDetector.ISSUE,
+            ScopedAssistedInjectedDetector.ISSUE,
             ScopedWithoutInjectAnnotationDetector.ISSUE,
         )
 
