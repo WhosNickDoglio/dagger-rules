21a64d10c856cdbbb4e39c7502de50b49f1597b5
nicholasdoglio <NicholasDoglio@Gmail.com>
2024-02-24 08:56:19 -0500

    More ktlint stuff

--- build-logic/src/main/kotlin/dev/whosnickdoglio/buildlogic/configuration/SpotlessConfiguration.kt
+++ build-logic/src/main/kotlin/dev/whosnickdoglio/buildlogic/configuration/SpotlessConfiguration.kt
@@ -9,36 +9,36 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 import org.gradle.api.Project
 
 internal fun Project.configureSpotless(ktlintVersion: String) {
-  pluginManager.apply("com.diffplug.spotless")
-  dependOnBuildLogicTask("spotlessCheck")
-  dependOnBuildLogicTask("spotlessApply")
-  extensions.getByType(SpotlessExtension::class.java).apply {
-    format("misc") { formatExtension ->
-      with(formatExtension) {
-        target("*.md", ".gitignore")
-        trimTrailingWhitespace()
-        endWithNewline()
-      }
-    }
+    pluginManager.apply("com.diffplug.spotless")
+    dependOnBuildLogicTask("spotlessCheck")
+    dependOnBuildLogicTask("spotlessApply")
+    extensions.getByType(SpotlessExtension::class.java).apply {
+        format("misc") { formatExtension ->
+            with(formatExtension) {
+                target("*.md", ".gitignore")
+                trimTrailingWhitespace()
+                endWithNewline()
+            }
+        }
 
-    kotlin { kotlinExtension ->
-      with(kotlinExtension) {
-        ktlint(ktlintVersion)
-        trimTrailingWhitespace()
-        endWithNewline()
-        licenseHeaderFile(rootProject.file("spotless/spotless.kt"))
-      }
+        kotlin { kotlinExtension ->
+            with(kotlinExtension) {
+                ktlint(ktlintVersion)
+                trimTrailingWhitespace()
+                endWithNewline()
+                licenseHeaderFile(rootProject.file("spotless/spotless.kt"))
+            }
+        }
+        kotlinGradle { kotlinGradleExtension ->
+            with(kotlinGradleExtension) {
+                ktlint(ktlintVersion)
+                trimTrailingWhitespace()
+                endWithNewline()
+                licenseHeaderFile(
+                    rootProject.file("spotless/spotless.kt"),
+                    "(import|plugins|buildscript|dependencies|pluginManagement)",
+                )
+            }
+        }
     }
-    kotlinGradle { kotlinGradleExtension ->
-      with(kotlinGradleExtension) {
-        ktlint(ktlintVersion)
-        trimTrailingWhitespace()
-        endWithNewline()
-        licenseHeaderFile(
-          rootProject.file("spotless/spotless.kt"),
-          "(import|plugins|buildscript|dependencies|pluginManagement)",
-        )
-      }
-    }
-  }
 }
