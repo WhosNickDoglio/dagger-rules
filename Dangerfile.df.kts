4547c139c5cb4929d6d7aa20e256263cb22bf31b
Nicholas Doglio <3968678+WhosNickDoglio@users.noreply.github.com>
2024-11-02 12:05:55 -0400

    Setup Danger (#402)
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue
    
    <!--- This project only accepts pull requests related to open issues -->
    
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    
    <!--- Please link to the issue here: -->
    
    ## Checklist
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have made corresponding changes to the documentation
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have mentioned changes in [CHANGELOG.md](../CHANGELOG.md).
    - [ ] I have read the [**CONTRIBUTING**](CONTRIBUTING.md) document.
    
    ## PR Stack
    
    <!-- branch-stack -->
    
    - `main`
      - \#402 :point\_left:

---
+++ Dangerfile.df.kts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2024 Nicholas Doglio
+ * SPDX-License-Identifier: MIT
+ */
+
+import systems.danger.kotlin.danger
+import systems.danger.kotlin.onGitHub
+import systems.danger.kotlin.warn
+
+danger(args) {
+    val allSourceFiles = git.modifiedFiles + git.createdFiles
+    val sourceChanges = allSourceFiles.filter { path -> path.contains("src") }
+
+    onGitHub {
+
+        // Ensure all major changes are mentioned in changelog
+        if (!allSourceFiles.contains("CHANGELOG.md") && sourceChanges.isNotEmpty()) {
+            warn(
+                "Any major changes should be added to our [`CHANGELOG.md`](CHANGELOG.md) file\n" +
+                    "Changelog format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/)."
+            )
+        }
+
+        // Ensure all PRs are small and easy to review
+        if ((pullRequest.additions ?: 0) - (pullRequest.deletions ?: 0) > 300) {
+            warn(
+                "This is a large pull request, can we break it down into multiple smaller PRs? " +
+                    "Considering [stacking](https://www.stacking.dev/) them if that helps"
+            )
+        }
+    }
+}
